0c5f852d460b8b1f3fb4317974b4acf145c900e5
Vincent Bonmalais <vb.kouno@gmail.com>
2013-08-16 16:04:05 +0400

    Fix spec.

--- spec/lib/hand_rules_spec.rb
+++ spec/lib/hand_rules_spec.rb
@@ -9,7 +9,7 @@ describe "Hands comparison" do
       first_hand << PokerHands::Card.new('8', 'H')
       first_hand << PokerHands::Card.new('8', 'C')
       first_hand << PokerHands::Card.new('7', 'H')
-      first_hand << PokerHands::Card.new('10', 'H')
+      first_hand << PokerHands::Card.new('T', 'H')
       first_hand << PokerHands::Card.new('A', 'H')
     end
 
@@ -18,7 +18,7 @@ describe "Hands comparison" do
       second_hand << PokerHands::Card.new('6', 'H')
       second_hand << PokerHands::Card.new('6', 'C')
       second_hand << PokerHands::Card.new('7', 'H')
-      second_hand << PokerHands::Card.new('10', 'H')
+      second_hand << PokerHands::Card.new('T', 'H')
       second_hand << PokerHands::Card.new('A', 'H')
       expect(first_hand).to be > second_hand
     end
