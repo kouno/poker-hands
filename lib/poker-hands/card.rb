961127ec8b728edb9c307f45b345f3e1073e6d4c
Vincent Bonmalais <vb.kouno@gmail.com>
2013-08-16 14:29:34 +0400

    Add constants for suits and values.

--- lib/poker-hands/card.rb
+++ lib/poker-hands/card.rb
@@ -1,5 +1,9 @@
 module PokerHands
   class Card
+    VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+    SUITS  = ['C', 'D', 'H', 'S']
+
     attr_accessor :value, :suit
+
   end
 end
--- spec/lib/card_spec.rb
+++ spec/lib/card_spec.rb
@@ -5,4 +5,12 @@ describe PokerHands::Card do
 
   it { should respond_to(:value) }
   it { should respond_to(:suit) }
+
+  it "defines a constant for cards values" do
+    expect(PokerHands::Card).to have_constant(:VALUES)
+  end
+
+  it "defines a constant for cards suits" do
+    expect(PokerHands::Card).to have_constant(:SUITS)
+  end
 end
