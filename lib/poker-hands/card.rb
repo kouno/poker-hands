7795456af90ed4f1b6c84fd9db7e6aa44e990446
Vincent Bonmalais <vb.kouno@gmail.com>
2013-08-16 14:39:25 +0400

    Make cards comparable.

--- lib/poker-hands/card.rb
+++ lib/poker-hands/card.rb
@@ -1,9 +1,23 @@
 module PokerHands
   class Card
+    include Comparable
+
     VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
     SUITS  = ['C', 'D', 'H', 'S']
 
     attr_accessor :value, :suit
 
+    def initialize(value = nil, suit = nil)
+      @value = value if VALUES.include?(value)
+      @suit  = suit  if SUITS.include?(suit)
+    end
+
+    def <=>(anOther)
+      VALUES.index(@value) <=> VALUES.index(anOther.value)
+    end
+
+    def inspect
+      "Card (#{@value}, #{suit})"
+    end
   end
 end
--- spec/lib/card_spec.rb
+++ spec/lib/card_spec.rb
@@ -13,4 +13,11 @@ describe PokerHands::Card do
   it "defines a constant for cards suits" do
     expect(PokerHands::Card).to have_constant(:SUITS)
   end
+
+  it "can be compared to another card" do
+    expect(PokerHands::Card.new('2', 'H')).to be < PokerHands::Card.new('3', 'H')
+    expect(PokerHands::Card.new('2', 'C')).to be < PokerHands::Card.new('3', 'H')
+    expect(PokerHands::Card.new('Q', 'C')).to be > PokerHands::Card.new('3', 'H')
+    expect(PokerHands::Card.new('A', 'C')).to be == PokerHands::Card.new('A', 'H')
+  end
 end
